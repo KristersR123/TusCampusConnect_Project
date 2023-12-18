f07878c216cb29d3f3ecf9cce073ad20d40d7269
Kristers <K00273773@student.tus.ie>
2023-12-18 21:00:24 +0000

    Master-Project

--- app/src/main/java/com/example/myapplication/auth/EventScreen.kt
+++ app/src/main/java/com/example/myapplication/auth/EventScreen.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication.auth
 
 import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -32,6 +33,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.myapplication.IgViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -39,15 +42,20 @@ import java.util.*
 @Composable
 fun EventScreen(navController: NavController, vm: EventViewModel) {
     var eventName by remember { mutableStateOf("") }
-    var eventDate by remember { mutableStateOf<Date?>(null) }
+    var eventDateTime by remember { mutableStateOf<Date?>(null) }
     var events by remember { mutableStateOf(emptyList<Pair<String, String>>()) } // List to store events
+
+    val user = FirebaseAuth.getInstance().currentUser
+    val userId = user?.uid
+    val eventsCollection = userId?.let {
+        FirebaseFirestore.getInstance().collection("users").document(it).collection("events")
+    }
 
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color(0xFF7E6E44))
+            .background(Color(0xFF7E6E44)) // Reverted to the original background color
             .padding(16.dp)
-
     ) {
         Column(
             modifier = Modifier
@@ -67,10 +75,10 @@ fun EventScreen(navController: NavController, vm: EventViewModel) {
                     .padding(bottom = 16.dp)
             )
 
-            // Event Date
+            // Event Date and Time
             DatePicker(
-                selectedDate = eventDate,
-                onDateSelected = { eventDate = it },
+                selectedDateTime = eventDateTime,
+                onDateTimeSelected = { eventDateTime = it },
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(56.dp)
@@ -80,58 +88,54 @@ fun EventScreen(navController: NavController, vm: EventViewModel) {
             // Add Event Button
             Button(
                 onClick = {
-                    // Handle adding event here
-                    if (!eventName.isBlank() && eventDate != null) {
-                        // Format the date if needed
-                        val formattedDate = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(eventDate!!)
-                        // Do something with the event details,  pass them to ViewModel
-                        vm.addEvent(eventName, formattedDate)
-                        // Reset the fields
+                    if (!eventName.isBlank() && eventDateTime != null) {
+                        val formattedDateTime = SimpleDateFormat("dd-MM-yyyy HH:mm", Locale.getDefault()).format(eventDateTime!!)
+                        vm.addEvent(eventName, formattedDateTime)
                         eventName = ""
-                        eventDate = null
+                        eventDateTime = null
                     } else {
                         // Show a message or handle the case when the input is not valid
                     }
                 },
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(56.dp),
+                    .height(56.dp)
+                    .clip(RoundedCornerShape(4.dp))
+                    .background(Color(0xFF6A5B3B)) // Darker than the background
+                    .padding(vertical = 8.dp),
                 colors = ButtonDefaults.textButtonColors(
-                    contentColor = Color(
-                        red = 0.02916666679084301f,
-                        green = 0.028315972536802292f,
-                        blue = 0.028315972536802292f,
-                        alpha = 1f
-                    )
+                    contentColor = contentColorFor(Color(0xFF6A5B3B)) // Text color darker than the button background
                 )
             ) {
                 Text(
                     text = "Add Event",
-                    fontSize = 18.sp
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold
                 )
             }
 
             // Display saved events
             val events by vm.events.collectAsState()
-            events.forEach { (name, date) ->
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 8.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ) {
-                    Text(text = "Event: $name")
-                    Text(text = "Date: $date")
+            if (events.isNotEmpty()) {
+                Column {
+                    events.forEach { (name, date) ->
+                        EventListItem(name = name, date = date)
+                        Spacer(modifier = Modifier.height(8.dp))
+                    }
                 }
+            } else {
+                Text("No events added yet.")
             }
+
             Spacer(modifier = Modifier.height(30.dp))
 
+            // Back Button
             Box(
                 modifier = Modifier
-                    .clip(RoundedCornerShape(50.dp))
+                    .clip(RoundedCornerShape(4.dp))
                     .background(
                         brush = Brush.horizontalGradient(
-                            colors = listOf(Color(0xFFFFD700), Color(0xFFFFD700))
+                            colors = listOf(Color(0xFF6A5B3B), Color(0xFF6A5B3B))
                         )
                     )
             ) {
@@ -143,17 +147,14 @@ fun EventScreen(navController: NavController, vm: EventViewModel) {
                         Color.Transparent
                     ),
                     modifier = Modifier
-                        .width(300.dp)
-                        .background(
-                            brush = Brush.horizontalGradient(
-                                colors = listOf(Color(0xff554800), Color(0xff554800))
-                            )
-                        )
+                        .fillMaxWidth()
+                        .height(56.dp)
+                        .clip(RoundedCornerShape(4.dp))
                 ) {
                     Text(
                         text = "Back",
                         color = Color.Black,
-                        fontSize = 30.sp,
+                        fontSize = 18.sp,
                         fontWeight = FontWeight.Bold
                     )
                 }
@@ -161,21 +162,15 @@ fun EventScreen(navController: NavController, vm: EventViewModel) {
         }
     }
 }
-
 @Composable
 fun DatePicker(
-    selectedDate: Date?,
-    onDateSelected: (Date) -> Unit,
+    selectedDateTime: Date?,
+    onDateTimeSelected: (Date) -> Unit,
     modifier: Modifier = Modifier
 ) {
     var showDialog by remember { mutableStateOf(false) }
 
-    Box(
-        modifier = Modifier
-
-
-
-    ) {
+    Box(modifier = modifier) {
         Button(
             onClick = { showDialog = true },
             modifier = Modifier
@@ -191,33 +186,63 @@ fun DatePicker(
             )
         ) {
             Text(
-                text = selectedDate?.let {
-                    SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(it)
-                } ?: "Select Date",
+                text = selectedDateTime?.let {
+                    SimpleDateFormat("dd-MM-yyyy HH:mm", Locale.getDefault()).format(it)
+                } ?: "Select Date and Time",
                 fontSize = 16.sp
             )
         }
 
         if (showDialog) {
             val context = LocalContext.current
-            val currentDate = selectedDate ?: Calendar.getInstance().time
-            val datePicker = remember { mutableStateOf(currentDate) }
+            val currentDate = selectedDateTime ?: Calendar.getInstance().time
+            var dateTimePicker by remember { mutableStateOf(currentDate) }
 
-            DatePickerDialog(
-                context,
-                { _, year, month, dayOfMonth ->
-                    val selectedDate = Calendar.getInstance().apply {
-                        set(Calendar.YEAR, year)
-                        set(Calendar.MONTH, month)
-                        set(Calendar.DAY_OF_MONTH, dayOfMonth)
-                    }.time
-                    datePicker.value = selectedDate
-                    onDateSelected(selectedDate)
-                },
-                datePicker.value.year + 1900,
-                datePicker.value.month,
-                datePicker.value.date
-            ).show()
+            Column {
+                // DatePicker
+                DatePickerDialog(
+                    context,
+                    { _, year, month, dayOfMonth ->
+                        dateTimePicker = Calendar.getInstance().apply {
+                            set(Calendar.YEAR, year)
+                            set(Calendar.MONTH, month)
+                            set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                        }.time
+                    },
+                    dateTimePicker.year + 1900,
+                    dateTimePicker.month,
+                    dateTimePicker.date
+                ).show()
+
+                // TimePicker
+                TimePickerDialog(
+                    context,
+                    { _, hourOfDay, minute ->
+                        dateTimePicker = Calendar.getInstance().apply {
+                            set(Calendar.HOUR_OF_DAY, hourOfDay)
+                            set(Calendar.MINUTE, minute)
+                        }.time
+                        onDateTimeSelected(dateTimePicker)
+                        showDialog = false
+                    },
+                    dateTimePicker.hours,
+                    dateTimePicker.minutes,
+                    false
+                ).show()
+            }
         }
+    }
+}
+
+@Composable
+fun EventListItem(name: String, date: String) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(text = "Event: $name", fontWeight = FontWeight.Bold)
+        Text(text = "Date: $date", color = Color.White)
     }
 }
--- app/src/main/java/com/example/myapplication/auth/EventViewModel.kt
+++ app/src/main/java/com/example/myapplication/auth/EventViewModel.kt
@@ -2,16 +2,59 @@ package com.example.myapplication.auth
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class EventViewModel : ViewModel() {
+    private val auth = FirebaseAuth.getInstance()
+    private val userId: String?
+        get() = auth.currentUser?.uid
+
+    private val eventsCollection: CollectionReference?
+        get() = userId?.let {
+            FirebaseFirestore.getInstance().collection("users").document(it).collection("events")
+        }
+
     private val _events = MutableStateFlow(emptyList<Pair<String, String>>())
     val events: StateFlow<List<Pair<String, String>>> = _events
 
-    fun addEvent(eventName: String, eventDate: String) {
-        val newEvent = Pair(eventName, eventDate)
-        _events.value = _events.value + newEvent
+    init {
+        // Listen for changes in Firestore and update the state
+        eventsCollection?.addSnapshotListener { snapshot, error ->
+            if (error != null) {
+                // Handle error
+                return@addSnapshotListener
+            }
+
+            val updatedEvents = snapshot?.documents?.mapNotNull {
+                val eventName = it["eventName"] as? String
+                val eventDateTime = it["eventDateTime"] as? String
+                if (eventName != null && eventDateTime != null) {
+                    Pair(eventName, eventDateTime)
+                } else {
+                    null
+                }
+            } ?: emptyList()
+
+            // Update the state
+            _events.value = updatedEvents
+        }
+    }
+    fun addEvent(eventName: String, eventDateTime: String) {
+        val newEvent = Pair(eventName, eventDateTime)
+
+        // Add the event to Firestore
+        eventsCollection?.add(mapOf(
+            "eventName" to eventName,
+            "eventDateTime" to eventDateTime
+        ))?.addOnSuccessListener {
+            // Successfully added to Firestore
+        }?.addOnFailureListener {
+            // Handle failure
+        }
     }
 }
